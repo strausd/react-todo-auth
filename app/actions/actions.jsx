9b461698a6f9d98485df41c90def8cd99da6d550
Daniel Straus <strausd@me.com>
2017-05-02 13:35:34 -0500

    Fix no todos message and fetch todos from firebase on app start

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -21,6 +21,26 @@ export var addTodo = (todo) => {
     };
 };
 
+export var startAddTodos = () => {
+    return (dispatch, getState) => {
+        var todosRef = firebaseRef.child('todos');
+
+        return todosRef.once('value').then((snapshot) => {
+            var todos = snapshot.val() || {};
+            var parsedTodos = [];
+
+            Object.keys(todos).forEach((todoId) => {
+                parsedTodos.push({
+                    id: todoId,
+                    ...todos[todoId]
+                });
+            });
+
+            dispatch(addTodos(parsedTodos));
+        });
+    };
+};
+
 export var startAddTodo = (text) => {
     return (dispatch, getState) => {
         var todo = {
--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -90,13 +90,16 @@ describe('Actions', () => {
         var testTodoRef;
 
         beforeEach((done) => {
-            testTodoRef = firebaseRef.child('todos').push();
+            var todosRef = firebaseRef.child('todos');
+            todosRef.remove().then(() => {
+                testTodoRef = firebaseRef.child('todos').push();
 
-            testTodoRef.set({
-                text: 'Something todo',
-                completed: false,
-                createdAt: 61451654
-            }).then(() => done());
+                return testTodoRef.set({
+                    text: 'Something todo',
+                    completed: false,
+                    createdAt: 61451654
+                })
+            }).then(() => done()).catch(done);
         });
 
         afterEach((done) => {
@@ -118,7 +121,22 @@ describe('Actions', () => {
                     completed: true
                 });
                 expect(mockActions[0].updates.completedAt).toExist();
-                
+
+                done();
+            }, done);
+        });
+
+        it('should populate todos and dispatch ADD_TODOS', (done) => {
+            const store = createMockStore({});
+            const action = actions.startAddTodos();
+
+            store.dispatch(action).then(() => {
+                const mockActions = store.getActions();
+
+                expect(mockActions[0].type).toEqual('ADD_TODOS');
+                expect(mockActions[0].todos.length).toEqual(1);
+                expect(mockActions[0].todos[0].text).toEqual('Something todo');
+
                 done();
             }, done);
         });
