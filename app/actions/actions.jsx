092c2eec3ec33a7214c726b018db5060fcf02c63
Daniel Straus <strausd@me.com>
2017-05-02 17:18:49 -0500

    Update todos location to include uid

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -21,9 +21,32 @@ export var addTodo = (todo) => {
     };
 };
 
+export var startAddTodo = (text) => {
+    return (dispatch, getState) => {
+        var todo = {
+            text,
+            completed: false,
+            createdAt: moment().unix(),
+            completedAt: null
+        };
+        var uid = getState().auth.uid;
+        var todoRef = firebaseRef.child(`users/${uid}/todos`).push(todo);
+
+        return todoRef.then(() => {
+            dispatch(addTodo({
+                ...todo,
+                id: todoRef.key
+            }));
+        }, (e) => {
+            console.log('Error adding todo to firebase', e)
+        });
+    };
+};
+
 export var startAddTodos = () => {
     return (dispatch, getState) => {
-        var todosRef = firebaseRef.child('todos');
+        var uid = getState().auth.uid;
+        var todosRef = firebaseRef.child(`users/${uid}/todos`);
 
         return todosRef.once('value').then((snapshot) => {
             var todos = snapshot.val() || {};
@@ -37,27 +60,6 @@ export var startAddTodos = () => {
             });
 
             dispatch(addTodos(parsedTodos));
-        });
-    };
-};
-
-export var startAddTodo = (text) => {
-    return (dispatch, getState) => {
-        var todo = {
-            text,
-            completed: false,
-            createdAt: moment().unix(),
-            completedAt: null
-        };
-        var todoRef = firebaseRef.child('todos').push(todo);
-
-        return todoRef.then(() => {
-            dispatch(addTodo({
-                ...todo,
-                id: todoRef.key
-            }));
-        }, (e) => {
-            console.log('Error adding todo to firebase', e)
         });
     };
 };
@@ -79,7 +81,8 @@ export var updateTodo = (id, updates) => {
 
 export var startToggleTodo = (id, completed) => {
     return (dispatch, getState) => {
-        var todoRef = firebaseRef.child(`todos/${id}`);
+        var uid = getState().auth.uid;
+        var todoRef = firebaseRef.child(`users/${uid}/todos/${id}`);
         var updates = {
             completed,
             completedAt: completed ? moment().unix() : null
