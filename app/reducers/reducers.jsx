514f9fe69d307aa7955a7d5cf7bdf79f18ac69d1
Daniel Straus <strausd@me.com>
2017-05-02 17:39:16 -0500

    Wipe todos on logout

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -41,6 +41,8 @@ export var todosReducer = (state = [], action) => {
                 ...state,
                 ...action.todos
             ];
+        case 'LOGOUT':
+            return [];
         default:
             return state;
     }
